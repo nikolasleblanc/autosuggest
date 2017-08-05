f4892f9c9aafd51d925c86b43ef7d44804c43cbd
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2017-08-05 16:31:58 -0400

    Initial commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Http } from '@angular/http';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name = new FormControl();
   title = 'app';
+  results;
+
+  constructor(private http: Http) {
+    this.results = this.name.valueChanges.switchMap(e => this.getResults(e))
+  }
+
+  getResults = (str) => {
+    return this.http.get(`search/${str}`).map(res => res.json());
+  }
 }
